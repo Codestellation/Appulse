cda7e2f487f8c9ed246a45892c6bd91d5787db24
Jury Soldatenkov <yurysold@gmail.com>
2019-10-27 15:20:30 +0300

    Added test for http source

--- src/Appulse.Tests/EnsureEditorConfigTaskTests.cs
+++ src/Appulse.Tests/EnsureEditorConfigTaskTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Text;
+using Microsoft.AspNetCore.Hosting;
 using NUnit.Framework;
 
 namespace Codestellation.Appulse.Tests
@@ -46,6 +47,27 @@ namespace Codestellation.Appulse.Tests
             };
 
             Assert.That(task.Execute(), Is.False);
+        }
+
+        [Test]
+        public void Should_return_true_if_local_editor_config_matches_remote()
+        {
+            var engine = new StubBuildEngine();
+            string localEditorConfig = Path.Combine(Directory.GetCurrentDirectory(), ".editorconfig");
+            File.WriteAllText(localEditorConfig, Resources.EditorConfigContent, Encoding.UTF8);
+
+            using (IWebHost host = Resources.CreateWebServer())
+            {
+                host.Start();
+                var task = new EnsureEditorConfigTask
+                {
+                    BuildEngine = engine,
+                    ReferenceEditorConfig = new Uri($"http://localhost:{Resources.Port}").ToString(),
+                    SolutionDir = Directory.GetCurrentDirectory()
+                };
+
+                Assert.That(task.Execute(), Is.True);
+            }
         }
     }
 }
--- src/Appulse.Tests/Resources.cs
+++ src/Appulse.Tests/Resources.cs
@@ -1,8 +1,31 @@
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
 namespace Codestellation.Appulse.Tests
 {
     public static class Resources
     {
+        public const int Port = 52334;
+
+        public class Startup
+        {
+            public void Configure(IApplicationBuilder app)
+                => app.Run(c => c.Response.WriteAsync(EditorConfigContent, Encoding.UTF8));
+        }
+
         public static readonly string EditorConfigContent = @"# http://editorconfig.org
 # it's a stub for testing purposes. Don't add here any values.";
+
+        public static IWebHost CreateWebServer() =>
+            new WebHostBuilder()
+                .UseKestrel(c =>
+                {
+                    c.AddServerHeader = false;
+                    c.ListenAnyIP(Port);
+                })
+                .UseStartup<Startup>()
+                .Build();
     }
 }
